2036a78cbb1262627eec4c2c75ca7e91a9521938
hashkitti001 <iJeddy5.0@gmail.com>
2024-03-09 00:15:07 +0100

    fix to save and reload methods

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from models.base_model import BaseModel
 import json
-
+import os
 class FileStorage:
     """A class that serializes instances to a JSON file and deserializes JSON file to instances"""
     __file_path = './file.json'
@@ -15,36 +15,18 @@ class FileStorage:
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
     def save(self):
-        """Serializes __objects to the JSON file path"""
-        serialized_objects = {}
-        
-        try:
-            if(self.__file_path is not None):
-               with open(self.__file_path, 'w', encoding='utf-8') as file:
-                   for key, obj in self.__objects.items():
-                       serialized_objects[key] = obj.to_dict()
-                   json.dump(serialized_objects, file)
-                 
-        except PermissionError:
-            print("Access denied. Check file permissions")
-        except FileNotFoundError:
-            print("This file doesn't exist on this drive. Check the file path")
-        except IsADirectoryError:
-            print("Path leads to a directory, not a file")
-        except OSError:
-            print("There is no space left on your drive")
+        """ Serializes __objects = {}"""
+        new_entry = {}
+        with open(self.__file_path, mode="w") as file:
+            for key, value in self.__objects.items():
+                new_entry[key] = value.to_dict()
+                """JSON encoder and decoder"""
+            json.dump(new_entry, file)
+
     def reload(self):
-        """Deserializes the JSON file to the _objects dictionary"""
-        if self.__file_path is not None:
-            try:
-                with open(self.__file_path, 'r', encoding='utf-8') as file:
-                    data = json.load(file)
-                    self._objects = {key: BaseModel(**value) for key, value in data.items()}
-                    file.close()
-            except FileNotFoundError:
-                print("File not found. Check the file path")
-                with open(self.__file_path, 'x', encoding='utf-8') as file:
-                     pass
-                     file.close()
-            except json.JSONDecodeError:
-                print("Error decoding JSON data")
+        """ Deserializes __objects = {}"""
+        if os.path.isfile(self.__file_path):
+            with open(self.__file_path, mode="r") as file:
+                for key, value in json.load(file).items():
+                    new_obj = eval(value["__class__"])(**value)
+                    self.__objects[key] = new_obj

aec565a0273338d852a0a393c246cecb11ba27f6
hashkitti001 <iJeddy5.0@gmail.com>
2024-03-11 15:41:12 +0100

    Wrote tests for Amenity module

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+'''Tests for BaseModel class'''
+import datetime
+import unittest
+from models import base_model
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+import os
+class TestBaseModel(unittest.TestCase):
+    '''start tests'''
+
+    def setUp(self):
+        '''Checks if an object can be created from the class'''
+        self.my_object = BaseModel()
+
+    def test_docStringsExist(self):
+        '''Checks if docstrings are present in every class'''
+        class_msg = "Class doesn't have a docstring"
+        self.assertIsNotNone(BaseModel.__doc__, class_msg)
+        meth_msg = "Method doesn't have a docstring"
+        
+    def test_instancehasid(self):
+        '''Checks if the instance of the BaseModel class has an id'''
+        self.assertIsNotNone(self.my_object.id)
+    
+    def test_idisunique(self):
+        '''Checks if the id on an objects is unique (differs from another)'''
+        inst_two = BaseModel()
+        self.assertNotEqual(self.my_object.id, inst_two.id)
+    
+    def test_iscorrecttype(self):
+        '''Checks if the attributes of the BaseModel object are the right data type'''
+        working_obj = self.my_object
+        self.assertIs(type(working_obj.id), str)
+        self.assertIs(type(working_obj.created_at), datetime.datetime)
+        self.assertIs(type(working_obj.updated_at), datetime.datetime)
+
+    def test_executable_file(self):
+        '''test if file has permissions u+x to execute'''
+        can_be_exec = os.access('models/base_model.py', os.X_OK)
+        self.assertTrue(can_be_exec)
+
+    def test_to_dict(self):
+        '''check if to_dict returns a dictionary, if add a class
+        key with class name of the object and if updated_at and
+        created_at are converted to string object in ISO format.'''
+        my_dict_model = self.my_object.to_dict()
+        self.assertIsInstance(my_dict_model, dict)
+        for key, value in my_dict_model.items():
+            flag = 0
+            if my_dict_model['__class__'] == 'BaseModel':
+                flag += 1
+            self.assertTrue(flag == 1)
+        for key, value in my_dict_model.items():
+            if key == 'created_at':
+                self.assertIsInstance(value, str)
+            if key == 'updated_at':
+                self.assertIsInstance(value, str)
+
+    def test_kwargs(self):
+        '''check when a dictionary in sent as **kwargs argument'''
+        self.my_object.name = "Everton"
+        self.my_object.my_number = 192
+        my_object_json = self.my_object.to_dict()
+        my_object_kwargs = BaseModel(**my_object_json)
+        self.assertNotEqual(my_object_kwargs, self.my_object)
+
+    def test_des_and_serialization(self):
+        '''check serialization and deserialization'''
+        storage = FileStorage()
+        all_objs = storage.all()
+        self.assertIsInstance(all_objs, dict, "a dictionary")  # Test all
+        self.my_object.name = "Plummage"
+        self.my_object.my_number = 3599
+        self.my_object.save()
+        with open("file.json", "r", encoding='utf-8') as f:
+            self.assertTrue(self.my_object.name in f.read())  # Test save
+if __name__ == "__main__":
+    unittest.main()
